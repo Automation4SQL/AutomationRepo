6717a8e7552f9ba735324ec919e3477382b5a86c
MCDinh <MCDinh@SQL198.SQLP.local>
2018-02-28 09:45:48 +0100

    added test case 3 - for Branch A

---
+++ Object Repository/Test Case 3 - for Branch A/Page_Entity Framework - Google-Such/a_Entity Framework  Wikipedia.rs
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Entity Framework  Wikipedia</name>
+   <tag></tag>
+   <elementGuidId>464eb94e-f754-484f-ab45-a8fbd89fc396</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>https://de.wikipedia.org/wiki/Entity_Framework</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ping</name>
+      <type>Main</type>
+      <value>/url?sa=t&amp;source=web&amp;rct=j&amp;url=https://de.wikipedia.org/wiki/Entity_Framework&amp;ved=0ahUKEwjioJSLnMjZAhUE2aQKHQT8DwgQFgg2MAI</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Entity Framework – Wikipedia</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;rso&quot;)/div[@class=&quot;_NId&quot;]/div[@class=&quot;g mnr-c g-blk&quot;]/div[@class=&quot;kp-blk _Z7 _Rqb _RJe&quot;]/div[@class=&quot;xpdopen&quot;]/div[@class=&quot;_OKe&quot;]/div[2]/div[@class=&quot;g&quot;]/div[1]/div[@class=&quot;rc&quot;]/h3[@class=&quot;r&quot;]/a[1]</value>
+   </webElementProperties>
+</WebElementEntity>
